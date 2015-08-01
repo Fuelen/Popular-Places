7acf9ed8263373fc956e4071140e6b53755dfd8f
Fuelen <ifuelen@gmail.com>
2015-08-01 17:36:55 +0300

    Add creation of a new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,27 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new place_params
+    respond_to do |format|
+      if @place.save
+        flash[:success] = "New place has been added"
+        format.html { redirect_to root_url }
+      else
+        format.html { render 'new' }
+      end
+    end
+
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name,:description,:image)
+  end
 end
