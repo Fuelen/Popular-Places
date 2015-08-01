715d5a5ed2f6ff15ee9d7b90366c795c57f558f6
Fuelen <ifuelen@gmail.com>
2015-08-01 19:59:57 +0300

    Refactor listing of flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_alert(message,type = :success)
+    content_tag :div, class: "alert alert-dismissable alert-#{type}" do
+      content_tag(:button, '&times;'.html_safe, class: :close,
+                  'data-dismiss': 'alert') + message
+    end
+  end
+
+  def list_flash_alerts
+    flash.map { |type, message| flash_alert message, type }.join.html_safe
+  end
 end
