c4420fc7afac97d7fd490985b342270f0be3809b
Artur Plysyuk <ifuelen@gmail.com>
2015-08-03 16:57:02 +0300

    Remove  html rendering  when create new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,36 +1,19 @@
 class PlacesController < ApplicationController
-  before_action :define_new_place, only: [:index, :new]
 
   def index
     @places = Place.order created_at: :desc
+    @place = Place.new
   end
 
-  def new
-  end
 
   def create
     @place = Place.new place_params
     respond_to do |format|
-      if @place.save
-        @flash_message = "New place has been added"
-        format.html do
-          flash[:success] = @flash_message
-          redirect_to root_url
-        end
-        format.js
-      else
-        format.html { render 'new' }
-        format.js   { render 'form'}
-      end
+      format.js   { render 'form' unless @place.save}
     end
-
   end
 
   private
-
-  def define_new_place
-    @place = Place.new
-  end
 
   def place_params
     params.require(:place).permit(:name,:description,:image)
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'places#index'
-  get 'new_place' => 'places#new'
   post 'places' => 'places#create'
 
   # Example of regular route:
